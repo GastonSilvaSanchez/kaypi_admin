6d49542efcea28ce0951cdbae28c7cbd9c185ed2
Axel <axel20ayalam@gmail.com>
2021-11-29 23:45:22 -0400

    sistema con nueva UI

--- controllers/usersController.js
+++
@@ -1,9 +0,0 @@
-const userService = require('../services/usuarioService');
-
-const consultUsers = async (req, res) => {
-    res.json({
-        usuario: await userService.getUsers()
-    });
-}
-
-module.exports = {consultUsers};
--- models/userModel.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require('mongoose');
-const userSchema = new Schema({
-    nombreUsuario:String,
-    contraseña: String
-});
-
-module.exports = model('Usuario', userSchema);
